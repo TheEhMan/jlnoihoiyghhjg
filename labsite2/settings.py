f8bfe1620431456ab471f1da9788a57fade45cb5
Castin Masika <castinmasikah@protonmail.com>
2023-02-13 16:56:59 +0300

    disable whitenoise shortly

--- labsite2/settings.py
+++ labsite2/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/mediafiles')
