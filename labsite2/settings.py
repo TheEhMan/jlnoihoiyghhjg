b25dcf8cdf803c3a53005a746a2f380fdb4fed41
Castin Masika <castinmasikah@protonmail.com>
2023-02-13 15:57:17 +0300

    no static files dirs when debug is off

--- labsite2/settings.py
+++ labsite2/settings.py
@@ -128,9 +128,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/mediafiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/'),
-]
+if DEBUG == True:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'staticfiles/'),
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
